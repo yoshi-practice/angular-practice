9217a6ccad8ca06543aec79e8d91a5cafdbf9bf5
y-kaino <yoppy1125yoppy@gmail.com>
2018-12-27 15:28:57 +0900

    [Update] ヒーローのモックを作成

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'User1'},
+    { id: 12, name: 'User2'},
+    { id: 13, name: 'User3'},
+    { id: 14, name: 'User4'},
+    { id: 15, name: 'User5'},
+    { id: 16, name: 'User6'},
+    { id: 17, name: 'User7'},
+    { id: 18, name: 'User8'},
+    { id: 19, name: 'User9'},
+    { id: 20, name: 'User10'},
+];
