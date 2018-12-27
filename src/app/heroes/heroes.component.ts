0fa183c808d76f84f5b64395e2a1e75dcca2b19a
y-kaino <yoppy1125yoppy@gmail.com>
2018-12-27 15:42:24 +0900

    [Update] ヒーローを装飾する

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,11 +7,14 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
 
   constructor() { }
 
